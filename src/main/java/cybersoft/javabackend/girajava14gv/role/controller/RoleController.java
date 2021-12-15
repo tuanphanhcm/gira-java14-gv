59b301f229b2b09adeea3ac7316d3325703cf978
Phan Thanh Tuan <tuan.phanth91@gmail.com>
2021-12-15 21:14:43 +0700

    setup role, group role

--- src/main/java/cybersoft/javabackend/girajava14gv/role/controller/RoleController.java
+++ src/main/java/cybersoft/javabackend/girajava14gv/role/controller/RoleController.java
@@ -1,5 +1,25 @@
 package cybersoft.javabackend.girajava14gv.role.controller;
 
-public interface RoleController {
+import java.util.List;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import cybersoft.javabackend.girajava14gv.role.model.Role;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "${api.role.name}", description = "${api.role.description}")
+public interface RoleController {
+	
+	
+	@Operation(method = "get", description = "get all roles")
+	@ApiResponses({
+			@ApiResponse(responseCode = "200", description = "get roles successfully"),
+			@ApiResponse(responseCode = "403", description = "do not have the correct authorization")
+	})
+	@GetMapping(value = "/roles", produces = "application/json")
+	public ResponseEntity<List<Role>> getRoles();
 }
