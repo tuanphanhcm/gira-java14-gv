bfd8b0921687ec2a33de68f1373130aa0057ddf4
Phan Thanh Tuan <tuan.phanth91@gmail.com>
2021-12-15 19:01:18 +0700

    fix open api configuration
    OpenApiConfiguration.java: add contact
    application.yml:  add api-docs path

--- src/main/java/cybersoft/javabackend/girajava14gv/config/OpenApiConfiguration.java
+++ src/main/java/cybersoft/javabackend/girajava14gv/config/OpenApiConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 
@@ -14,13 +15,18 @@ public class OpenApiConfiguration {
 	@Bean
 	public OpenAPI getOpenApi() {
 		return new OpenAPI()
-				.info(new Info().title("Gira Application")
-	              .description("Spring shop sample application")
-	              .version("v1.0")
-	              .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+				.info(new Info()
+					.title("Gira Application")
+					.description("Gira Application for Education Purpose")
+					.version("v1.0")
+					.license(new License().name("NO LICENSE").url("http://tuanphan.dev"))
+					.contact(new Contact()
+	            		  .email("contact@tuanphan.dev")
+	            		  .name("Phan Thanh Tuan")
+	            		  .url("https://tuanphan.dev")))
 			    .externalDocs(new ExternalDocumentation()
-	              .description("SpringShop Wiki Documentation")
-	              .url("https://springshop.wiki.github.org/docs"));
+	              .description("Spring Documentation")
+	              .url("https://docs.spring.io/spring-framework/docs/current/reference/html/"));
 
 	}
 }
